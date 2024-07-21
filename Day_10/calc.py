a82eb8f038fcdcc63f70687a6a17f475a4ba9c51
Abhishek Patel <abhishek.patel@shl.com>
2024-07-21 20:15:00 +0530

    how to optimize code

---
+++ Day_10/calc.py
@@ -0,0 +1,38 @@
+import art
+# print(art.logo)
+
+def calc(first_num,sec_num,opr):
+    if opr == "+":
+        return first_num+sec_num
+    elif opr == "-":
+        return first_num-sec_num
+    elif opr == "*":
+        return first_num*sec_num
+    elif opr == "/":
+        return first_num/sec_num
+    
+flag = False
+
+while not flag:
+    num1 = int(input("What is first number? :  "))
+    print("+","\n-","\n*","\n/")
+    opr = input("Pick an operation: ")
+    num2 = int(input("What is your next number? : "))
+    result = calc(num1,num2,opr)
+    print(f"{num1} {opr} {num2 } = {result}")
+
+    should_countinue = input(f"Type 'y' to countinue with {result}, ot type no to start a new calculation.")
+    
+    next_flag = True
+    while next_flag:
+        if should_countinue == "y":
+            opr = input("Pick an operation: ")
+            num2 = int(input("What is your next number? : "))
+            result = calc(result,num2,opr)
+            print(f"{num1} {opr} {num2 } = {result}")
+
+            should_countinue = input(f"Type 'y' to countinue with {result}, ot type no to start a new calculation.")
+
+            if should_countinue == 'no':
+                next_flag = False
+
---
+++ Day_10/v1_calc.py
@@ -0,0 +1,49 @@
+from art import logo
+
+
+def add(num1,num2):
+    return num1+num2
+
+def mul(num1,num2):
+    return num1*num2
+
+def subs(num1,num2):
+    return num1-num2
+
+def div(num1,num2):
+    return num1/num2
+
+operations = {
+    "+" : add,
+    "-" : subs,
+    "*" : mul,
+    "/" : div
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? : "))
+
+    for sysmbol in operations:
+        print(sysmbol)
+
+    should_countinue = True
+
+    while should_countinue:
+        operation_symbol = input("Pick an operation : ")
+        num2 = float(input("what's the next number? : "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to countinue calculation with {answer}, or type n to start new calculation ") == 'y':
+                num1 = answer
+        
+        else:
+            should_countinue = False
+            calculator()
+
+calculator()
+
+
